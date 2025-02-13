5a09fc70cf0d17384dd481c15d90c558940bb29d
AymenDellas <aymen.dellas@etu.usthb.dz>
2025-02-13 19:27:46 +0100

    final route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-          placeholder: "admin@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const user = {
-          id: "1",
-          name: "Admin",
-          email: "test@test",
-          password: "test",
-        };
-
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Missing email or password");
-        }
-
-        if (
-          credentials.email !== user.email ||
-          credentials.password !== user.password
-        ) {
-          throw new Error("Invalid email or password");
-        }
-
-        return { id: user.id, name: user.name, email: user.email };
-      },
-    }),
-  ],
-  pages: { signIn: "/auth/login" },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-        token.name = user.name;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.email = token.email;
-        session.user.name = token.name;
-      }
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  session: { strategy: "jwt", maxAge: 7 * 24 * 60 * 60 },
-};
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/auth.config";
 
 const handler = NextAuth(authOptions);
 
